32302bd75c375aee9c7afed8dc439fa66cb2f65f
Yang Cheng <b05705014@ntu.edu.tw>
2019-05-15 14:52:59 +0800

    heroku test

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ var path = require('path');
 
 const API_PORT = 3001;
 const app = express();
+app.use(cors());
 app.use(express.static(path.join(__dirname, "../client/build")));
 const router = express.Router();
 
@@ -63,10 +64,52 @@ router.get("/getMembers", (req, res) => {
 
 router.get("/getPhotos", (req, res) => {
     Photo.find((err, data) => {
+
         if (err) return res.json({ success: false, error: err });
         return res.json({ success: true, data: data });
     });
 });
+
+// // this is our update method
+// // this method overwrites existing data in our database
+// router.post("/updateData", (req, res) => {
+//   const { id, update } = req.body;
+//   Data.findOneAndUpdate(id, update, err => {
+//     if (err) return res.json({ success: false, error: err });
+//     return res.json({ success: true });
+//   });
+// });
+
+// // this is our delete method
+// // this method removes existing data in our database
+// router.delete("/deleteData", (req, res) => {
+//   const { id } = req.body;
+//   Data.findOneAndDelete(id, err => {
+//     if (err) return res.send(err);
+//     return res.json({ success: true });
+//   });
+// });
+
+// // this is our create methid
+// // this method adds new data in our database
+// router.post("/putData", (req, res) => {
+//   let data = new Data();
+
+//   const { id, message } = req.body;
+
+//   if ((!id && id !== 0) || !message) {
+//     return res.json({
+//       success: false,
+//       error: "INVALID INPUTS"
+//     });
+//   }
+//   data.message = message;
+//   data.id = id;
+//   data.save(err => {
+//     if (err) return res.json({ success: false, error: err });
+//     return res.json({ success: true });
+//   });
+// });
 
 // append /api for our http requests
 app.use("/api", router);
